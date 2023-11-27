2b223da9c44ad64719d9da2e9ef28cd6d032ba75
Kimberly Spencer <kimberly.liz@sga.pucminas.br>
2023-11-27 08:26:14 -0300

    Create DTO

---
+++ src/main/java/com/webtech/labapi/domain/product/RequestProduct.java
@@ -0,0 +1,13 @@
+package com.webtech.labapi.domain.product;
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+//DTO
+//objeto somente para transferencia de dados (DTO -data transfer object)
+public record RequestProduct(
+        String id,
+        @NotBlank String name,
+        @NotNull float price) {
+}
