781834f520688e777419b5773e03e5787ac5ac96
Kimberly Spencer <kimberly.liz@sga.pucminas.br>
2023-11-27 08:15:53 -0300

    initial config

---
+++ src/main/java/com/webtech/labapi/LabapiApplication.java
@@ -0,0 +1,13 @@
+package com.webtech.labapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webtech/labapi/LabapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webtech.labapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
